b69eea3d06365a6dba8d77cc089a6caff984e204
Let-Borges <leandreatoborges@gmail.com>
2025-09-12 04:06:57 +0000

    Adicionei comentarios

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    char estado, codigo_carta[4], cidade[20]; //
+
+    //variaveis para a 1° carta
+
+    char estado, codigo_carta[4], cidade[20]; 
     int populacao, pontos_turisticos;
     float area, pib;
 
-    printf("Carta 1 \n");
+    //Indica que as informações de baixo são da 1° carta
+
+    printf("Carta 1 \n"); 
+
+    //A partir daqui ira aparecer no terminal as caracteristicas da 1° carta para o usuario preencher
 
     printf("Estado: ");
     scanf("%c", &estado);
@@ -28,6 +35,8 @@ int main(){
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontos_turisticos);
 
+    //Imprime na tela os dados da 1° carta que foi preenchida pelo usuario
+
     printf("\nCarta 1 \n");
     printf("Estado: %c \n", estado);
     printf("Código: %s \n", codigo_carta);
@@ -37,11 +46,17 @@ int main(){
     printf("PIB: %.2f bilhões de reais \n", pib);
     printf("Número de Pontos Turíticos: %d \n\n", pontos_turisticos);
 
+    //variaveis para a 2° carta
+
     char estado2, codigo_carta2[4], cidade2[20];
     int populacao2, pontos_turisticos2;
     float area2, pib2;
 
+    //Indica que as informações de baixo são da 2° carta
+
     printf("Carta 2 \n");
+
+    //A partir daqui ira aparecer no terminal as caracteristicas da 2° carta para o usuario preencher
 
     printf("Estado: ");
     scanf(" %c", &estado2);
@@ -63,6 +78,8 @@ int main(){
 
     printf("Número de Ponto Turísticos: ");
     scanf("%d", &pontos_turisticos2);
+
+    //Imprime na tela os dados da 2° carta que foi preenchida pelo usuario
 
     printf("\nCarta 2 \n");
     printf("Estado: %c \n", estado2);
