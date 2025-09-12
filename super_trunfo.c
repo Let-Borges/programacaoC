57bfd0b9783ee9901a37b567f6472141cbae0143
Let-Borges <leandreatoborges@gmail.com>
2025-09-12 03:38:58 +0000

    Cadastro 2 carta

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    char estado, codigo_carta[4], cidade[20]; 
+    char estado, codigo_carta[4], cidade[20]; //
     int populacao, pontos_turisticos;
     float area, pib;
 
@@ -37,7 +37,41 @@ int main(){
     printf("PIB: %f \n", pib);
     printf("Número de Pontos Turíticos: %d \n\n", pontos_turisticos);
 
+    char estado2, codigo_carta2[4], cidade2[20];
+    int populacao2, pontos_turisticos2;
+    float area2, pib2;
+
     printf("Carta 2 \n");
+
+    printf("Estado: ");
+    scanf(" %c", &estado2);
+
+    printf("Código: ");
+    scanf("%s", &codigo_carta2);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", &cidade2);
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Número de Ponto Turísticos: ");
+    scanf("%d", &pontos_turisticos2);
+
+    printf("\nCarta 2 \n");
+    printf("Estado: %c \n", estado2);
+    printf("Código: %s \n", codigo_carta2);
+    printf("Nome da Cidade: %s \n", cidade2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %f km² \n", area2);
+    printf("PIB: %f \n", pib2);
+    printf("Número de Pontos Turíticos: %d", pontos_turisticos2);
 
     return 0;
 }
